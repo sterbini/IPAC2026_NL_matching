c502f9736da2f94b82b2ca3c125ad981d22d5452
Guido Sterbini <guido.sterbini@cern.ch>
2025-11-24 21:35:51 +0100

    Status

--- LHC_injection/001_load_json.py
+++ LHC_injection/001_load_json.py
@@ -3,10 +3,22 @@ import xtrack as xt
 import matplotlib.pyplot as plt
 
 line = xt.load('lhc_injection.json')
+
 # %%
 tw = line.twiss(method='4d')
 
 # %%
 plt.plot(tw.s, tw.betx, label='betx')
 
+# %%
+line.twiss(method='4d', at_s=30)
+line['on_x1_v']=111
+# %%
+collider = xt.Environment()
+
+collider.import_line(line=line,
+                             line_name='lhcb1')
+
+# %%
+collider['on_x1_v']
 # %%
